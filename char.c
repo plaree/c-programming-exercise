b4921c90c4870a9a95bd47d82a6cff10efbee4ee
Eralp Demirer <eralp0814@gmail.com>
2023-01-28 22:36:48 +0300

    Create char.c
    i should recreate it with a loop

---
+++ char.c
@@ -0,0 +1,13 @@
+/*#include<stdio.h>
+
+int main(void){
+    char letter;
+    scanf("%c", &letter);
+    printf("++++%c++++\n+++%c%c%c+++\n++%c%c%c%c%c++\n+%c%c%c%c%c%c%c+\n%c%c%c%c%c%c%c%c%c", letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter, letter);
+    
+    return 0;
+}
+*/
+
+// write this code with for loop
+

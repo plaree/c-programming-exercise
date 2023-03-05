d25c8968e37369ea3abbaa55cc7451b383718448
Eralp Demirer <eralp0814@gmail.com>
2023-03-05 15:46:17 +0300

    Create branch inside a loop how many big cities.c

---
+++ branch inside a loop how many big cities.c
@@ -0,0 +1,60 @@
+/*
+You want to determine the number of cities in a given region that have a population strictly greater than 10,000. To do this, you write a program that first reads the number of cities in a region as an integer, and then the populations for each city one by one (also integers).
+
+Example
+Input
+6
+1000
+5000
+15000
+4780
+0
+23590
+Output
+2
+*/
+
+#include<stdio.h>
+
+int main(void){
+    
+    int num_city;
+    int pop;
+    int i;
+    int counter = 0;
+    scanf("%d", &num_city);
+    
+    for(i=0;i<num_city;i++){
+        scanf("%d", &pop);
+        
+        if(pop>10000){
+            counter++;
+        }    
+        
+    }
+    
+    printf("%d", counter);
+    
+    return 0;
+}
+    
+// ------------------ SOLUTION OF THE TUTORS -----------------------------------
+
+#include <stdio.h>
+
+int main(void){
+    int nbCities;
+    scanf("%d", &nbCities);
+    int nbBigCities = 0;
+    for(int i = 0; i < nbCities ; i++)
+    {
+        int population;
+        scanf("%d", &population);
+        if(population > 10 * 1000)
+        {
+            nbBigCities = nbBigCities + 1;
+        }
+    }
+    printf("%d", nbBigCities);
+    return 0;
+}

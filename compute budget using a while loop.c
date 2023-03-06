9b73091573a7abcc5dd172454bc935898b989766
Eralp Demirer <eralp0814@gmail.com>
2023-03-06 21:13:54 +0300

    Update compute budget using a while loop.c

--- compute budget using a while loop.c
+++ compute budget using a while loop.c
@@ -51,3 +51,23 @@ int main(void){
     }
     return 0;
 }
+
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int expense;
+    scanf("%d",&expense);
+    int sumOfExpenses = 0;
+    while (expense != -1)
+    {
+        sumOfExpenses = sumOfExpenses + expense;
+        scanf("%d",&expense);
+    }
+    printf("%d\n",sumOfExpenses);
+
+    return 0;
+}
+*/

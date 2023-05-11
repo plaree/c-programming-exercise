92a110adff1448fd447dd0bd6c237079688f6120
Eralp Demirer <eralp0814@gmail.com>
2023-05-11 21:30:47 +0300

    Create round off numbers and circumvent errors.c

---
+++ 3/3.2/round off numbers and circumvent errors.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    float num = 0.25;
+    
+    printf("the number is %.40f\n", num);
+    
+    double num2 = 0.25;
+    
+    printf("the number 2 is %.40f\n", num2);
+
+    float num3 = 0.3;
+    
+    printf("the number 3 is %.40f\n", num3);
+
+    float num4 = 0.3;
+    
+    printf("the number 4 is %.40f\n", num4);
+    
+    return(0);
+
+}

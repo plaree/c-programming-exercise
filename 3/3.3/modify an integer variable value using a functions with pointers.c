7e74bacb43e28a5f389970c6f8c110f76f0a206a
Eralp Demirer <eralp0814@gmail.com>
2023-06-19 22:13:01 +0300

    Update modify an integer variable value using a functions with pointers.c

---
+++ 3/3.3/modify an integer variable value using a functions with pointers.c
@@ -0,0 +1,32 @@
+// here is another example... 
+
+#include <stdio.h>
+void addThree(int);
+int main() {
+
+    int a = 5;
+    addThree(a);
+    printf("inside main, value of a: %d\n",a);
+    return 0;
+}
+void addThree(int a){
+    a = a + 3;
+    printf("inside addThree, value of a: %d\n",a);
+}
+//------------------------------------------------------
+
+/*#include <stdio.h>
+void addThree(int *);
+int main() {
+    //! showMemory(start=65520)
+    int a = 5;
+    addThree(&a);
+    printf("inside main, value of a: %d\n",a);
+    return 0;
+}
+void addThree(int * a){
+    *a = *a + 3;
+    printf("inside addThree, value of a: %d\n",*a);
+}
+
+*/
